4c34aa5601e6e080b32af2c2f34a7388ebeecda3
Jacob Knudsen <jknuds19@student.aau.dk>
2021-03-15 14:56:35 +0100

    Yes

--- src/jaco/scripts/jacoScript.py
+++ src/jaco/scripts/jacoScript.py
@@ -28,22 +28,7 @@ def run():
         messagejoint5 = Float64()
         messagejoint6 = Float64()
 
-        for x in range(10):
-            messagejoint1.data = x
-            messagejoint2.data = x
-            messagejoint3.data = x
-            messagejoint4.data = x
-            messagejoint5.data = x
-            messagejoint6.data = x
-
-            joint1pub.publish(messagejoint1)
-            joint2pub.publish(messagejoint2)
-            joint3pub.publish(messagejoint3)
-            joint4pub.publish(messagejoint4)
-            joint5pub.publish(messagejoint5)
-            joint6pub.publish(messagejoint6)
-            R.sleep()
-        for x in range(10, 0, -1):
+        for x in range(6):
             messagejoint1.data = x
             messagejoint2.data = x
             messagejoint3.data = x
