805d5044be14555928a276b9bc17d73e1ceb39ee
abmoRobotics <abmo19@student.aau.dk>
2021-03-26 10:33:16 +0100

    deleted a file

--- src/shapefitting/src/tester.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <ros/ros.h>
-int main(int argc, char **argv){
-     ros::init(argc, argv, "talker");
-    ros::NodeHandle node;
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
-    return 0;
-}
