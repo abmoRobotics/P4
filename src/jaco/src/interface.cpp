945800fa27ec9c9e3224df9528b902e164bc165a
mariebogestrand <mbages19@student.aau.dk>
2021-03-30 11:30:32 +0200

    Interface v2

--- src/jaco/src/interface.cpp
+++ src/jaco/src/interface.cpp
@@ -1,22 +1,48 @@
 #include <ros/ros.h>
 #include <iostream>
-#include <vision/Detection.h>
+//#include <vision/Detection.h>
+#include <actionlib/client/simple_action_client.h>
+//#include <shapefitting/shapefitting_positionAction.h> 
 
 using namespace std;
 
 ros::NodeHandle nh;
 
-vector<> Classes;
+vector<int16_t> objects;
 
-void fullcauto_callback(const vision::Detection_arrayConstPtr& msg){
-
-
+void fullauto_callback(const vision::Detection_arrayConstPtr& msg){
+    for (size_t i = 0; i < 10; i++){
+        objects.push_back(msg);
+    }
 }
+
+
+
+
 // Få objekter fra JJ, udvælg et, send dette objekt til Emil, send Emils data til pos_callback i jaco_trajectory.
 void full_automatic(){
-//Subscribe to JJ Detection.msg. Get Class and print.
+    cout << "Choose an object: " << endl;
+    for (size_t i = 0; i < sizeof(objects)/sizeof(objects[0]); i++){
+        //cout << i << objects[i].class << endl;
+    }
+    int obj_select;
+    cin >> obj_select;
+    //Send object to ShapeFitting
+    actionlib::SimpleActionClient<shapefitting::shapefitting_positionAction> shapefitting_client("get_shape", true); 
+    //Check input in actionclient
+    shapefitting_client.waitForServer();
+    //shapefitting::shapefitting_positionInput input;
+    shapefitting_client.sendGoal(obj_select);
 
-//Service
+    //Action for trajectory and grasping
+    
+
+    //Feedback on distance to object
+
+
+
+    //Goal  = object grasped?
+
 
 }
 //Automation begynder ved en bestemt distance til et objekt.
@@ -57,7 +83,15 @@ void menu(){
 }
 
 
-ros::Subscriber fullauto_sub = nh.subscribe<vision::Detection_array>("/Vision/ObjectDetection", 10, fullauto_callback());
+ros::Subscriber fullauto_sub = nh.subscribe<vision::Detection_array>("/Vision/ObjectDetection", 10, fullauto_callback);
+
+
+actionlib::SimpleActionClient<shapefitting::shapefitting_positionAction> shapefitting_client("get_shape", true); 
+//Check input in actionclient
+shapefitting_client.waitForServer();
+
+
+
 
 void main(int argc, char *argv[]){
     ros::init(argc, argv, "interface");
