746e0aafd8fab0285a39220188d0112af347d562
mariebogestrand <mbages19@student.aau.dk>
2021-03-31 10:16:31 +0200

    Interface version 123

--- src/jaco/src/interface.cpp
+++ src/jaco/src/interface.cpp
@@ -1,39 +1,38 @@
 #include <ros/ros.h>
 #include <iostream>
 #include <vision/detect_srv.h>
+#include <vision/Detection_array.h>
 #include <actionlib/client/simple_action_client.h>
 #include <jaco/IF_fullAutoAction.h> 
+#include <jaco/RAWItongueOut.h>
+#include <time.h>
 
 using namespace std;
 vector<vision::Detection> objects;
+int itongue_sensor;
+vision::Detection_array visionDataArray;
 
 void full_automatic(){
-    ros::NodeHandle nh;
-    ros::ServiceClient dclient = nh.serviceClient<vision::detect_srv>("detection_service");
-    vision::detect_srv srv_detection;
-    
-    if (dclient.call(srv_detection)){
-        int l = srv_detection.response.msg.size();
-        for (size_t i = 0; i < l ; i++){
-            cout << "Object no." << i << "\n" << srv_detection.response.msg[i] << endl;
+    int length = sizeof(visionDataArray.msg)/sizeof(visionDataArray.msg[0]);
+        for (size_t i = 0; i < length; i++){
+            cout << "Object no." << i << "\n" << visionDataArray.msg[i].Class << endl;
         }
-    }
     
     cout << "Choose an Object: " << endl;
-    int select_object;
-    cin >> select_object;
     jaco::IF_fullAutoActionPtr acPrt;
     actionlib::SimpleActionClient<jaco::IF_fullAutoAction> aclient("IF_full_auto", true); 
     jaco::IF_fullAutoGoal goal;
-    goal.goalObject = srv_detection.response.msg[select_object];
-    aclient.sendGoal(goal);
+    for (size_t i = 0; i < length; i++) {
+        if (itongue_sensor == i+1){
+        goal.goalObject = visionDataArray.msg[i];
+        aclient.sendGoal(goal);
+        aclient.waitForResult();
+    }   }
     
-    aclient.waitForResult();
+
     if (aclient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
         cout << "Robot have reached the object." << endl;
     }
-
-    menu();
 
 }
 
@@ -42,34 +41,62 @@ void semi_automatic(){
 
 }
 
+void itongue_callback(const jaco::RAWItongueOutConstPtr& msg){
+
+
+    int old_Sensor, Sensor_count;
+    if (old_Sensor == msg->Sensor){
+        Sensor_count ++;
+    }
+    else Sensor_count = 0;
+    old_Sensor = msg->Sensor;
+
+    // To get the sensor activated
+    if (Sensor_count > 4){
+        itongue_sensor = old_Sensor;
+    }    
+    else itongue_sensor = 0;
+}
+
+void vision_callback(const vision::Detection_arrayConstPtr& msg){
+        for (size_t i = 0; i < sizeof(msg->msg)/sizeof(msg->msg[0]); i++)
+    {
+        vision::Detection DetectionData;
+        DetectionData = msg->msg[i];
+        visionDataArray.msg.push_back(DetectionData);
+    }
+}
+
 
 void menu(){
 
-    int selection = 1;
+   // int selection = 1;
 
-    while (selection != 0){
+    // while (selection != 0){
         system("clear");
         cout << "_____________________________" << endl;
         cout << "1. Full Automatic Control Mode" << endl;
         cout << "2. Semi Automatic Control Mode" << endl;
         cout << "Select Control Mode" << endl;
         cout << "----------------------------" << endl;
-        cin >> selection;
 
-        switch (selection)
-        {
-        case 1:
-            full_automatic();
-            break;
-        case 2:
-            semi_automatic();
-            break;
 
-        default:
-            break;
-        }
-        selection = 0;
-    }
+        // cin >> selection;
+
+        // switch (selection)
+        // {
+        // case 1:
+        //     full_automatic();
+        //     break;
+        // case 2:
+        //     semi_automatic();
+        //     break;
+
+        // default:
+        //     break;
+        // }
+    //     selection = 0;
+    // }
     
 
 }
@@ -79,7 +106,26 @@ int main(int argc, char *argv[]){
     ros::init(argc, argv, "interface");
     ros::AsyncSpinner spinner(0);
     spinner.start();
-    menu();
+    time_t start = time(NULL);
+    time_t now = time(NULL);
+    //Run starting menu for 20 seconds
+    while ((now - start) <= 20){
+        menu();
+        time_t now = time(NULL);
+    }    
+
+    // while ((now - start) >= 21){
+    //     ros::NodeHandle nh;
+    // ros::Subscriber itongue_sub = nh.subscribe<jaco::RAWItongueOut>("/RAWItongueOut", 1, itongue_callback);
+        
+    // }
+    
+    ros::NodeHandle nh;
+    ros::Subscriber itongue_sub = nh.subscribe<jaco::RAWItongueOut>("/RAWItongueOut", 1, itongue_callback);
+    ros::Subscriber vision_sub = nh.subscribe<vision::Detection_array>("/Vision/ObjectDetection", 1, vision_callback);
+
+    ros::spinOnce();
+    
     ros::waitForShutdown();
     //ros::spin();
     return 0;
